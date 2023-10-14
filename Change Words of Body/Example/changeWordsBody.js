a34d24570e53fa991c7b9092a59e3063b903de68
Juan Pablo <juanpablosierra36@gmail.com>
2023-10-14 17:12:12 +0200

    Function "ChangeWordsBody" implemented

---
+++ Change Words of Body/Example/changeWordsBody.js
@@ -0,0 +1,21 @@
+"use strict"
+//By JScorpioDv
+//¡¡¡EXAMPLE!!!
+
+//FUNCTION
+function changeWordsBody(){
+    let body = document.body;
+    let newBody = body.innerHTML;
+    let existWord = 'sexo';
+    let newWord = '<span class="blocked-content">"Contenido Bloqueado"</span>';
+
+    //This "while" searches "existWord" in the body and replaces the words until "indexOf" is "-1".
+    while (newBody.indexOf(existWord) !== -1) {
+        newBody = newBody.replace(existWord, newWord);
+    }
+
+    body.innerHTML = newBody; //This line replaces the body for the newBody.
+}
+
+//PLAY
+changeWordsBody();
