7eba557719874905df20c579fdfe70d0712948e5
Juan Pablo <juanpablosierra36@gmail.com>
2023-11-01 21:09:29 +0100

    Create generatorHexadecimalColor.js

---
+++ Generator Hexadecimal Color/generatorHexadecimalColor.js
@@ -0,0 +1,12 @@
+"use strict"
+//By ChatGPT
+
+//FUNCTION
+function generateColorRGB() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+export { generateColorRGB };
