0ef8a25ce41ba6d21a33d71b1e0c548227f57e51
Juan Pablo <juanpablosierra36@gmail.com>
2023-11-13 10:29:34 +0100

    function "Add Notification" updated

--- Add Notification/addNotification.js
+++ Add Notification/addNotification.js
@@ -2,13 +2,19 @@
 //By JScorpioDv
 
 //FUNCTIONS
-function addNotification(notification, whereToPlaceIt){
+/**
+ * 
+ * @param {string} message 
+ * @param {Element HTML} whereToPlaceIt 
+ * @param {string} position {"beforebegin" , "afterbegin", "beforeend", "afterend"}
+ */
+function addNotification(position, whereToPlaceIt, message){
     const notification = document.createElement("h3");
-    notification.textContent = notification;
+    notification.textContent = message;
     notification.id = "notification";
     
     //We assign the notification after of the "whereToPlaceIt" element.
-    whereToPlaceIt.insertAdjacentElement("afterend", error);
+    whereToPlaceIt.insertAdjacentElement(position, notification);
 
     //I configure a timer to delete the object after two seconds half past.
     setTimeout(() => {
